c9916fcca038cc31ac601fcb690080f066017b6e
Denison Kolling <denisonkolling@gmail.com>
2023-10-13 15:13:51 -0300

    feat: add patient list

--- src/pages/Medicos/Medicos.jsx
+++ src/pages/Medicos/Medicos.jsx
@@ -114,69 +114,129 @@ const Medicos = () => {
 							<label for="nome" className="form-label">
 								Nome
 							</label>
-							<input type="text" className='form-control' name="nome" onChange={handleChange} value={medic.nome} />
+							<input
+								type="text"
+								className="form-control"
+								name="nome"
+								onChange={handleChange}
+								value={medic.nome}
+							/>
 						</div>
 
 						<div className="col-md-6">
 							<label for="inputEmail4" className="form-label">
 								Email
 							</label>
-							<input type="email" className="form-control" name="email" onChange={handleChange} value={medic.email}  />
+							<input
+								type="email"
+								className="form-control"
+								name="email"
+								onChange={handleChange}
+								value={medic.email}
+							/>
 						</div>
 
 						<div className="col-md-4">
 							<label for="telefone" className="form-label">
 								Telefone
 							</label>
-							<input type="number" className="form-control" name="telefone" onChange={handleChange} value={medic.telefone}  />
+							<input
+								type="number"
+								className="form-control"
+								name="telefone"
+								onChange={handleChange}
+								value={medic.telefone}
+							/>
 						</div>
-
+						{/* TODO: Implementar Select que carrega as opções do ENUM de Especilidades */}
 						<div className="col-md-4">
 							<label for="nome" className="form-label">
 								Especialidade
 							</label>
-							<input type="text" className='form-control' name="especialidade" onChange={handleChange} value={medic.especialidade} />
+							<input
+								type="text"
+								className="form-control"
+								name="especialidade"
+								onChange={handleChange}
+								value={medic.especialidade}
+							/>
 						</div>
 
 						<div className="col-md-4">
 							<label for="crm" className="form-label">
 								CRM
 							</label>
-							<input type="number" className="form-control" name="crm" onChange={handleChange} value={medic.crm}  />
+							<input
+								type="number"
+								className="form-control"
+								name="crm"
+								onChange={handleChange}
+								value={medic.crm}
+							/>
 						</div>
 
 						<div className="col-8">
 							<label for="endereco" className="form-label">
 								Endereço
 							</label>
-							<input type="text" className="form-control" name="logradouro" onChange={handleAddressChange} value={medic.endereco.logradouro}  />
+							<input
+								type="text"
+								className="form-control"
+								name="logradouro"
+								onChange={handleAddressChange}
+								value={medic.endereco.logradouro}
+							/>
 						</div>
 
 						<div className="col-2">
 							<label for="numero" className="form-label">
 								Número
 							</label>
-							<input type="text" className="form-control" name="numero" onChange={handleAddressChange} value={medic.endereco.numero}/>
+							<input
+								type="text"
+								className="form-control"
+								name="numero"
+								onChange={handleAddressChange}
+								value={medic.endereco.numero}
+							/>
 						</div>
-						
+
 						<div className="col-2">
 							<label for="complemento" className="form-label">
 								Complemento
 							</label>
-							<input type="text" className="form-control" name="complemento" onChange={handleAddressChange} value={medic.endereco.complemento}/>
+							<input
+								type="text"
+								className="form-control"
+								name="complemento"
+								onChange={handleAddressChange}
+								value={medic.endereco.complemento}
+							/>
 						</div>
 
 						<div className="col-8">
 							<label for="endereco" className="form-label">
 								Bairro
 							</label>
-							<input type="text" className="form-control" name="bairro" onChange={handleAddressChange} value={medic.endereco.bairro}  />
+							<input
+								type="text"
+								className="form-control"
+								name="bairro"
+								onChange={handleAddressChange}
+								value={medic.endereco.bairro}
+							/>
 						</div>
 						<div className="col-md-6">
 							<label for="cidade" className="form-label">
 								Cidade
 							</label>
-							<input type="text" className="form-control" name="cidade" onChange={handleAddressChange} value={medic.endereco.cidade}/>
+							<input
+								type="text"
+								className="form-control"
+								name="cidade"
+								onChange={handleAddressChange}
+								value={medic.endereco.cidade}
+							/>
 						</div>
 
 						{/* //TODO: Alterar campo UF para Select dos estados onde existem unidades. */}
@@ -196,14 +256,26 @@ const Medicos = () => {
 							<label for="cep" className="form-label">
 								Estado
 							</label>
-							<input type="text" className="form-control" name="uf" onChange={handleAddressChange} value={medic.endereco.uf} />
+							<input
+								type="text"
+								className="form-control"
+								name="uf"
+								onChange={handleAddressChange}
+								value={medic.endereco.uf}
+							/>
 						</div>
 
 						<div className="col-md-2">
 							<label for="cep" className="form-label">
 								CEP
 							</label>
-							<input type="text" className="form-control" name="cep" onChange={handleAddressChange} value={medic.endereco.cep} />
+							<input
+								type="text"
+								className="form-control"
+								name="cep"
+								onChange={handleAddressChange}
+								value={medic.endereco.cep}
+							/>
 						</div>
 
 						{/* //TODO: incluir mensagem de sucesso quando registro for inserido no Banco de Dados */}
--- src/pages/Pacientes/Pacientes.jsx
+++ src/pages/Pacientes/Pacientes.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import pacienteService from '../../service/paciente.service';
 
 const Pacientes = () => {
+	const [patientList, setPatientList] = useState([]);
+	const [msg, setMsg] = useState('');
+	const [patient, setPatient] = useState({
+		nome: '',
+		email: '',
+		telefone: '',
+		endereco: {
+			logradouro: '',
+			bairro: '',
+			cep: '',
+			numero: '',
+			complemento: '',
+			cidade: '',
+			uf: '',
+		},
+	});
+
+	useEffect(() => {
+		init();
+	}, []);
+
+	const init = () => {
+		pacienteService
+			.getAllPaciente()
+			.then((res) => {
+				setPatientList(res.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+
 	return (
 		<main className="container-fluid">
-			<div className='d-flex'>
+			<div className="d-flex">
 				<aside
 					style={{
 						width: '202px',
@@ -14,9 +47,39 @@ const Pacientes = () => {
 					}}>
 					<Navbar />
 				</aside>
-        <div className="container-fluid">
-          <h1>Pacientes</h1>
-        </div>
+				<div className="container-fluid">
+					<h1>Pacientes</h1>
+
+					{/* Início -- Tabela Listagem de Pacientes */}
+
+				<div className="container mt-2">
+					<h4>Listagem de Pacientes</h4>
+					<table className="table table-striped">
+						<thead>
+							<tr>
+								<th scope="col">#</th>
+								<th scope="col">Nome</th>
+								<th scope="col">Email</th>
+								<th scope="col">Telefone</th>
+								<th scope="col">Cidade</th>
+							</tr>
+						</thead>
+						<tbody>
+							{patientList.map((p) => (
+								<tr key={p.id}>
+									<td>{p.id}</td>
+									<td>{p.nome}</td>
+									<td>{p.email}</td>
+									<td>{p.telefone}</td>
+									<td>{p.endereco.cidade}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+
+				{/* Fim -- Tabela Listagem de Pacientes */}
+				</div>
 			</div>
 		</main>
 	);
