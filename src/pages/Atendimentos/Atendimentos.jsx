5b9a31be8b9970ccb6c622e07ff43948d5c8de7a
Denison Kolling <denisonkolling@gmail.com>
2023-11-02 08:12:56 -0300

    feat: create queue service page

--- src/pages/Atendimentos/Atendimentos.jsx
+++ src/pages/Atendimentos/Atendimentos.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import appointmentService from '../../service/appointment.service';
+import { App, Calendar } from 'react-bootstrap-icons';
 
 const Atendimentos = () => {
 	const [appointmentList, setAppointmentList] = useState([]);
@@ -74,8 +75,87 @@ const Atendimentos = () => {
 					<Navbar />
 				</aside>
 
-				<div className="container-fluid" style={{border: '1px solid red'}}>
-					<h4>Fila de Atendimentos</h4>
+				<div className="container-fluid" style={{ border: '1px solid red' }}>
+					<div className="d-flex">
+						<div className="card col-3 m-3 p-3 px-4 flex-grow-1">
+							<App className="bi me-2" width="16" height="16"></App>
+							<div>
+								<span className="text-start fw-bold">
+									Dr. Edward Scheckelton
+								</span>
+								<br />
+								<span className="text-start fw-light">
+									Clínica Santa Helena
+								</span>
+							</div>
+						</div>
+						<div className="card col-4 m-3 py-3 px-4 flex-grow-1">
+							<Calendar className="bi me-2" width="16" height="16"></Calendar>
+							<span className="text-start fw-bold">Hoje</span>
+							<span className="text-start">02 NOV 2023</span>
+						</div>
+						<div className="card col-2 m-3 p-3 fw-bold">
+							<span className="text-end">Agendamentos</span>
+						</div>
+						<div className="card col-2 m-3 p-3 fw-bold">
+							<span className="text-end">Encaixes</span>
+						</div>
+					</div>
+
+					<div class="card text-center flex-grow-1 m-3">
+						<div class="card-header">
+							<ul class="nav nav-tabs card-header-tabs">
+								<li class="nav-item">
+									<span class="nav-link active">
+										Fila<br/>
+										<span className='fw-bold'>05</span>
+									</span>
+								</li>
+								<li class="nav-item">
+									<p class="nav-link" href="#">
+										Antecipados
+									</p>
+								</li>
+								<li class="nav-item">
+									<a
+										class="nav-link"
+										href="#"
+										tabindex="-1"
+										aria-disabled="true">
+										Aguardando
+									</a>
+								</li>
+								<li class="nav-item">
+									<a
+										class="nav-link"
+										href="#"
+										tabindex="-1"
+										aria-disabled="true">
+										Cancelamentos
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="card-body">
+							<div className="container-fluid mt-2">
+								<table className="table table-striped">
+									<thead>
+										<tr>
+											<th scope="col">#</th>
+											<th scope="col">Medico</th>
+											<th scope="col">Especialidade</th>
+											<th scope="col">Paciente</th>
+											<th scope="col">Data</th>
+										</tr>
+									</thead>
+								</table>
+							</div>
+
+							<a href="#" class="btn btn-primary">
+								Go somewhere
+							</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</main>
